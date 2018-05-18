4725b6209970102be45da558c6910ae2659b88a5
saifur rahman <saifur@Shantas-MacBook-Air.local>
2018-05-18 04:29:36 -0400

    nav fixes

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "-250px";
+  document
+    .getElementsByClassName("openbtn")
+    .item(0)
+    .setAttribute("id", "clicked");
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("clicked").style.display = "auto";
+  document.getElementById("clicked").removeAttribute("id");
 }
